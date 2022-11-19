3257c8793e721fa6ed63d7a075e6b75c8cef06bb
Aaron Tacke <tacke@in.tum.de>
2022-11-19 11:37:10 +0100

    Added header

---
+++ components/header.js
@@ -0,0 +1,25 @@
+import { Image, View, StyleSheet } from 'react-native';
+
+// List of facts
+const header_source = require('../assets/header.png')
+
+// Shows random facts (mockup using images)
+export default function Header() {
+    return (
+        <View style={styles.headerView}>
+            <Image style={styles.headerLogo} source={header_source} />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    headerView: {
+        paddingBottom: 30,
+        width: '100%',
+        alignItems: 'center',
+    },
+    headerLogo: {
+        height: 100,
+        width: 320,
+    },
+});
