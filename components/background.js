93fc2516c804d279cf757e64fcb62ab03f104f97
Aaron Tacke <tacke@in.tum.de>
2022-11-19 11:37:24 +0100

    Added background

---
+++ components/background.js
@@ -0,0 +1,21 @@
+import { ImageBackground, Dimensions, StyleSheet } from 'react-native';
+
+const background = require('../assets/background.png');
+const screenHeight = Dimensions.get('window').height;
+const screenWidth = Dimensions.get('window').width;
+
+// Shows random facts (mockup using images)
+export default function Background({ children }) {
+    return (
+        <ImageBackground source={background} style={styles.background} resizeMode="stretch">
+            {children}
+        </ImageBackground>
+    );
+}
+
+const styles = StyleSheet.create({
+    background: {
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
